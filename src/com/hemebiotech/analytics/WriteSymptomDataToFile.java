5e6319c273622fc21f46166dc185769d67e045fc
Amaryllis Pittin <amaryllispittin@gmail.com>
2025-06-20 18:29:38 +0200

    c corection défaut import

--- src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,5 +1,26 @@
 package com.hemebiotech.analytics;
 
-public class WriteSymptomDataToFile {
+public class WriteSymptomDataToFile implements ISymptomWriter {
+	
+	private String outputFile;
+	
+	public WriteSymptomDataToFile(String outputFile) {
+		
+		this.outputFile = outputFile;
+		
+	}
+	
+	@Override
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		
+		try (FileWriter writer = new FileWriter(outputFile)) {
+			for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+				writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 }
