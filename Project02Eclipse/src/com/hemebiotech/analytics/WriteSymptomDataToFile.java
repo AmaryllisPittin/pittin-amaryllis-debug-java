4cd51904bf88e66a86a1a1399c8ad36500e794f7
Amaryllis Pittin <amaryllispittin@gmail.com>
2025-06-26 13:41:02 +0200

    result.out generated

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -0,0 +1,31 @@
+package com.hemebiotech.analytics;
+
+import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map.Entry;
+
+public class WriteSymptomDataToFile implements ISymptomWriter {
+	
+	private String outputFile;
+	
+	public WriteSymptomDataToFile(String outputFile) {
+		
+		this.outputFile = outputFile;
+		
+	}
+	
+	@Override
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		
+		try (FileWriter writer = new FileWriter(outputFile)) {
+			for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+				writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
