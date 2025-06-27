af02302ed6cc9bf7134e2f85f8151bd8462f67ba
Amaryllis Pittin <amaryllispittin@gmail.com>
2025-06-27 12:29:16 +0200

    Etape 4 : création du constructeur dans AnalyticsCounter - FONCTIONNE

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,69 +1,43 @@
-<<<<<<< HEAD
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.FileWriter;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
 public class AnalyticsCounter {
 	
-	public static void main(String args[]) throws Exception {
+	private ISymptomReader reader;
+	private ISymptomWriter writer;
+	
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		
+		this.reader = reader;
+		this.writer = writer;
+	}
+	
+	public void analyzeSymptoms() {
+		
+		List<String> symptoms = reader.GetSymptoms();	
 		Map<String, Integer> symptomCounts = new HashMap<>();
 		
-		System.out.println("Lecture du fichier symptoms.txt...");
-		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-		
-		while(line != null) {
-			symptomCounts.put(line, symptomCounts.getOrDefault(line, 0) + 1);
-			line = reader.readLine();
+		for(String symptom : symptoms) {
+			
+			symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) + 1);
+			
 		}
-		reader.close();
-		System.out.println("Lecture terminée. Nombre de symptômes : " + symptomCounts.size());
 		
-		System.out.println("Écriture dans result.out...");
-		ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
 		writer.writeSymptoms(symptomCounts);
-		System.out.println("Fichier écrit !");
-	}
 
+	}
+	
+	public static void main(String[] args) {
+		
+		ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
+		ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
+		
+		AnalyticsCounter counter = new AnalyticsCounter(reader, writer);
+		counter.analyzeSymptoms();
+		
+	}
 	
 }
-=======
-package com.hemebiotech.analytics;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.FileWriter;
-import java.util.Map;
-import java.util.HashMap;
-
-public class AnalyticsCounter {
-	
-	public static void main(String args[]) throws Exception {
-		Map<String, Integer> symptomCounts = new HashMap<>();
-		
-		System.out.println("Lecture du fichier symptoms.txt...");
-		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-		
-		while(line != null) {
-			symptomCounts.put(line, symptomCounts.getOrDefault(line, 0) + 1);
-			line = reader.readLine();
-		}
-		reader.close();
-		System.out.println("Lecture terminée. Nombre de symptômes : " + symptomCounts.size());
-		
-		System.out.println("Écriture dans result.out...");
-		ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
-		writer.writeSymptoms(symptomCounts);
-		System.out.println("Fichier écrit !");
-	}
-
-	
-}
->>>>>>> dev-latest
--- README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-# Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application
-=======
-Sample project for OpenClassroom's fundamentals of Java
->>>>>>> dev-latest
